449ebe26a7c6b3e96570301a6554d7ad1e5b6901
Jacob Collins <jtc00002@mix.wvu.edu>
2024-09-02 16:15:37 -0400

    Initial commit

---
+++ media/lua/server/items/AlienPistolDistributions.lua
@@ -0,0 +1,13 @@
+require 'Items/ProceduralDistributions'
+
+table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, "LAP.Alien");
+table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, 100);
+
+table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, "LAP.Alien");
+table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, 100);
+
+table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, "LAP.Alien");
+table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, 100);
+
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, "LAP.Alien");
+table.insert(ProceduralDistributions["list"]["ArmyStorageGuns"].items, 100);
