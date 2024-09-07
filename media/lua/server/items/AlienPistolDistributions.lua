20df72a8d7166d53884f3809f3bc9d11e708c624
Jacob Collins <jtc00002@mix.wvu.edu>
2024-09-07 18:20:20 -0400

    Tweaked spawnrates and removed some locations

--- media/lua/server/items/AlienPistolDistributions.lua
+++ media/lua/server/items/AlienPistolDistributions.lua
@@ -1,54 +1,43 @@
 require 'Items/ProceduralDistributions'
 
-/* =========== GUN STORES ============ */	
-
-table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["GunStoreShelf"].items, 100);
-
 table.insert(ProceduralDistributions["list"]["GunStoreDisplayCase"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["GunStoreDisplayCase"].items, 100);
+table.insert(ProceduralDistributions["list"]["GunStoreDisplayCase"].items, 10);
 
 table.insert(ProceduralDistributions["list"]["PawnShopGuns"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["PawnShopGuns"].items, 100);
+table.insert(ProceduralDistributions["list"]["PawnShopGuns"].items, 4);
 
 table.insert(ProceduralDistributions["list"]["PawnShopGunsSpecial"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["PawnShopGunsSpecial"].items, 100);
-
-/* =========== CIVILIAN OWNERS ============ */	
+table.insert(ProceduralDistributions["list"]["PawnShopGunsSpecial"].items, 14);
 
 table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 100);
+table.insert(ProceduralDistributions["list"]["BedroomSideTable"].items, 0.03);
 
 table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 100);
+table.insert(ProceduralDistributions["list"]["BedroomDresser"].items, 0.04);
 
 table.insert(ProceduralDistributions["list"]["DresserGeneric"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["DresserGeneric"].items, 100);
+table.insert(ProceduralDistributions["list"]["DresserGeneric"].items, 0.04);
 
 table.insert(ProceduralDistributions["list"]["GarageFirearms"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["GarageFirearms"].items, 100);
+table.insert(ProceduralDistributions["list"]["GarageFirearms"].items, 5);
 
 table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 100);
+table.insert(ProceduralDistributions["list"]["KitchenRandom"].items, 0.01);
 
 table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 100);
+table.insert(ProceduralDistributions["list"]["OfficeDeskHome"].items, 0.04);
 
 table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, 100);
-
-/* =========== STASHES ============ */	
+table.insert(ProceduralDistributions["list"]["SecurityLockers"].items, 5);
 
 table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, 100);
+table.insert(ProceduralDistributions["list"]["PlankStashGun"].items, 8);
 
 table.insert(ProceduralDistributions["list"]["DrugLabGuns"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["DrugLabGuns"].items, 100);
+table.insert(ProceduralDistributions["list"]["DrugLabGuns"].items, 8);
 
 table.insert(ProceduralDistributions["list"]["DrugShackWeapons"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["DrugShackWeapons"].items, 100);
-
-/* =========== MISCELLANEOUS ============ */	
+table.insert(ProceduralDistributions["list"]["DrugShackWeapons"].items, 6);
 
 table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, "LAP.Alien");
-table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, 100);
+table.insert(ProceduralDistributions["list"]["FirearmWeapons"].items, 5);
